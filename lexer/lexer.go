cab48bf8dac040a0de984b69058a3e76d680d8c3
thoughtquery <thoughtquery@gmail.com>
2017-12-12 23:25:30 -0800

    Beginings of Lexer -- Testing wont work because go cant find the fucking package

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,5 +1,65 @@
-
-
-
 // take source code as input and output the tokens that represent the source code
 // go through input and output the next token it recognizes
+
+package lexer
+
+import "monkey/token"
+
+type Lexer struct {
+	input        string
+	position     int  // current position in input (after current char)
+	readPosition int  // current reading position in input (after current char)
+	ch           byte // current char under examination
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+// Give us the next character and advance our input in the input string
+func (l *Lexer) readChar() {
+	// Are we at the end of the input?
+	if l.readPosition >= len(l.input) {
+		// ASCII code for 'NUL' byte (Haven't read anything yet/End Of File)
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	// Always point from position were going to read from next
+	l.readPosition += 1
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACKET, l.ch)
+	case '}':
+		tok = newToken(token.RBRACKET, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+	l.reachChar()
+	return tok
+}
+
+func newToken(tokenType token.tokenType, ch byte) token.tokenType {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -1,19 +1,39 @@
 package lexer
 
 import (
-	"testing"
 	"monkey/token"
+	"testing"
 )
 
 func TestNextToken(t *testing.T) {
-	input := `=+(){},;`	
+	input := `=+(){},;`
 
 	tests := []struct {
-		expectedType 	token.TokenType
+		expectedType    token.TokenType
 		expectedLiteral string
 	}{
-		{TOKEN.ASSIGN, "="}	
-	
+		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBRACKET, "{"},
+		{token.RBRACKET, "}"},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
 	}
 
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong, expected=%q, got=%q")
+		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
+				i, tt.expectedLiteral, tok.Literal)
+		}
+	}
 }
